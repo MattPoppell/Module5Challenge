ba3d45278ae2f098ed6e8bd7528efd459d519017
Mathew Poppell <mpoppell1002@gmail.com>
2023-11-05 07:58:38 -0500

    added local storage/save button part

--- script.js
+++ script.js
@@ -34,3 +34,28 @@ function displayDate() {
       };
     });
   };
+
+$(function () {
+    $(".saveBtn").on("click", function () {
+      var hourText = $(this).siblings(".description").val();
+      var blockHour = $(this).parent().attr("id");
+      localStorage.setItem(blockHour, hourText);
+    });
+  
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
+    plannerStateChange();
+  
+  });
+  
+  displayDate();
+  setInterval(displayDate, 1000);
+  
